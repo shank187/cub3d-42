413fdb7b9a946e05842286e6325cd6b8d9e1e947
Aymane Elbour <aelbour@e3r7p8.1337.ma>
2025-07-23 10:12:45 +0100

    testing flood search

--- parsing_utils.c
+++ parsing_utils.c
@@ -6,46 +6,55 @@
 /*   By: aelbour <aelbour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 10:04:28 by aelbour           #+#    #+#             */
-/*   Updated: 2025/07/22 13:36:10 by aelbour          ###   ########.fr       */
+/*   Updated: 2025/07/23 10:09:42 by aelbour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
+
+int is_walkable(char c)
+{
+	return (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W');
+}
+
 void flood_fill(char **arr, int i, int j, t_game *game)
 {
-	if(! game->map.is_valid)
+	if (!game->map.is_valid)
 		return;
-	printf("%c i = %i | j = %i\n", arr[i][j], i, j);
-	arr[i][j] = 'x';
-	
-	if (arr[i + 1] && arr[i + 1][j] == '0')
-		flood_fill(arr, i + 1, j, game);
-	else if (arr[i + 1] && arr[i + 1][j] != '1' && arr[i + 1][j] != 'x')
-		game->map.is_valid = 0; 
-	
-	if (i && arr[i - 1][j] == '0')
-		flood_fill(arr, i - 1, j, game);
-	else if (i && arr[i - 1][j] != '1' && arr[i - 1][j] != 'x')
-		game->map.is_valid = 0;
-	
-	if (arr[i][j + 1] && arr[i][j + 1] == '0')
-		flood_fill(arr, i, j + 1, game);
-	else if (arr[i][j + 1] && arr[i][j + 1] != '1' && arr[i][j + 1] != 'x')
-		game->map.is_valid = 0;
 
-	if (j && arr[i][j - 1] == '0')
-		flood_fill(arr, i, j - 1, game);
-	else if (j && arr[i][j - 1] != '1' && arr[i][j - 1] != 'x')
-		game->map.is_valid = 0; 
+	if (arr[i] == NULL || arr[i][j] == '\0' || arr[i][j] == ' ')
+	{
+		game->map.is_valid = 0;
+		return;
+	}
+
+	if (arr[i][j] == '1' || arr[i][j] == 'x')
+		return;
+
+	if (!is_walkable(arr[i][j]))
+	{
+		game->map.is_valid = 0;
+		return;
+	}
+
+	arr[i][j] = 'x';
+
+	flood_fill(arr, i + 1, j, game);
+	flood_fill(arr, i - 1, j, game);
+	flood_fill(arr, i, j + 1, game);
+	flood_fill(arr, i, j - 1, game);
 }
+
+
 
 
 int main(void)
 {
 	t_game game;
+
 	game.map.is_valid = 1;
-	char **arr = ft_split("111111\n100f11\n000000\nfrnjlfr", '\n');
-	flood_fill(arr, 1, 3, &game);
+	char **arr = ft_split("010110\n100N01\n100001\n111111\n", '\n');
+	flood_fill(arr, 1, 3, &game); 
 	printf("%i\n", game.map.is_valid);
 }
