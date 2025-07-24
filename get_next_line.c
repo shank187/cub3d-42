c955be93adef84084338dcac9f73f02eb725044a
Aymane Elbour <aelbour@e3r7p8.1337.ma>
2025-07-24 08:32:13 +0100

    gnl rework

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aelbour <aelbour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 15:28:47 by aelbour           #+#    #+#             */
-/*   Updated: 2025/07/24 08:17:55 by aelbour          ###   ########.fr       */
+/*   Updated: 2025/07/24 08:28:26 by aelbour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ char *get_next_line(int fd)
 	char		*line;
 	int			i;
 
+	i = 0;
 	if (!str)
 	{
 		str = malloc(BUFFER_SIZE + 1);
@@ -41,19 +42,13 @@ char *get_next_line(int fd)
 		return (NULL);
 	if (read(fd, line, 0) == -1)
 		return (free(line), free(str), str = NULL, NULL);
-	while(i = read(fd, line, BUFFER_SIZE))
+	i = read(fd, line, BUFFER_SIZE);
+	while(i)
 	{
 		line[i] = 0;
 		tmp = str;
 		str = ft_strjoin(str, line);
 		free(tmp);
-		if(i == 0 && str && str[i])
-		{
-			tmp = line;
-			line = str;
-			free(tmp);
-			str = NULL;
-		}
 		if (ft_strchr(str, '\n'))
 		{
 			tmp = line;
@@ -64,7 +59,17 @@ char *get_next_line(int fd)
 			free(tmp);
 			return(line);
 		}
+		i = read(fd, line, BUFFER_SIZE);
 	}
+	if(i == 0 && str && str[i])
+	{
+		tmp = line;
+		line = str;
+		free(tmp);
+		str = NULL;
+		return (line);
+	}
+	return (NULL);
 }
 
 
