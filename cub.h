4338e5e5ca821ceddded33743248546be82f84b9
Abdelhamid El Haddadi <abel-had@e3r10p5.1337.ma>
2025-08-24 10:52:01 +0100

    norm

--- cub.h
+++ cub.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abel-had <abel-had@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 10:50:46 by abel-had          #+#    #+#             */
+/*   Updated: 2025/08/24 10:50:47 by abel-had         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB_H
 # define CUB_H
 
@@ -9,10 +21,8 @@
 # include <limits.h>
 # include <stdint.h>
 # include "mlx.h"
-# include <math.h>
 # include <stdbool.h>
 # include <errno.h>
-
 
 # define SCREEN_WIDTH 1280
 # define SCREEN_HEIGHT 720
@@ -29,21 +39,20 @@
 # define KEY_RIGHT 65363
 
 //macos keys
-# define MLX_KEY_W       13
-# define MLX_KEY_A       0
-# define MLX_KEY_S       1
-# define MLX_KEY_D       2
-# define MLX_KEY_LEFT   123
-# define MLX_KEY_RIGHT  124
+# define MLX_KEY_W 13
+# define MLX_KEY_A 0
+# define MLX_KEY_S 1
+# define MLX_KEY_D 2
+# define MLX_KEY_LEFT 123
+# define MLX_KEY_RIGHT 124
 
 # define BUFFER_SIZE 1000
 # define COLLISION_MARGIN 0.2
 
 # define WALL_COLOR 0xFFFFFF
-# define FLOOR_COLOR 0x000000 
+# define FLOOR_COLOR 0x000000
 # define PLAYER_COLOR 0xFF0000
 # define DOOR_COLOR 0x8B4513
-// #define MINIMAP_S 
 
 typedef struct s_horse
 {
@@ -69,7 +78,8 @@ typedef struct s_horse
 	char	*texture;
 }	t_horse;
 
-typedef struct s_img {
+typedef struct s_img
+{
 	void	*img;
 	char	*addr;
 	int		bpp;
@@ -92,11 +102,6 @@ typedef struct s_player
 	double	y;
 	char	dir;
 	double	angle;
-	
-	//exe
-	
-	// double	pos_x;
-	// double	pos_y;
 	double	dir_y;
 	double	dir_x;
 	double	plane_x;
@@ -104,8 +109,8 @@ typedef struct s_player
 	char	start_dir;
 }	t_player;
 
-
-typedef struct s_ray {
+typedef struct s_ray
+{
 	bool	door;
 	double	cam_x;
 	double	ray_dir_x;
@@ -141,44 +146,41 @@ typedef struct s_textures
 	char	*we;
 	char	*ea;
 	char	*door;
-	// 
 }	t_textures;
 
 typedef struct s_door
 {
-	int x;
-	int y;
-	struct s_door *next;
-} t_door;
+	int				x;
+	int				y;
+	struct s_door	*next;
+}	t_door;
 
 typedef struct s_game
 {
-	t_player		player;// p	
+	t_player		player;
 	t_map			map;
 	t_horse			horse;
 	unsigned int	floor_color;
 	unsigned int	ceiling_color;
 	int				slide_left;
 	int				slide_right;
-	void		*mlx;
-	void		*win;
-	char		**arr;
-	t_img		img;
-	// t_player	p;
-	t_textures	tex;
-	t_img		texs[5];
-	// char		**map;
-	int			map_w;
-	int			map_h;
-	int			scr_w;
-	int			scr_h;
-	int			key_w;
-	int			key_a;
-	int			key_s;
-	int			key_d;
-	int			key_l;
-	int			key_r;
-	t_door		*closed_door;
+	void			*mlx;
+	void			*win;
+	char			**arr;
+	t_img			img;
+	t_textures		tex;
+	t_img			texs[5];
+	int				map_w;
+	int				map_h;
+	int				scr_w;
+	int				scr_h;
+	int				key_w;
+	int				key_a;
+	int				key_s;
+	int				key_d;
+	int				key_l;
+	int				key_r;
+	t_door			*closed_door;
 }	t_game;
 
 typedef struct s_gnl
@@ -189,7 +191,6 @@ typedef struct s_gnl
 	char	*tmp;
 	t_game	*game;
 }	t_gnl;
-
 
 typedef struct s_split
 {
@@ -214,19 +215,15 @@ typedef struct s_minimap
 	int	dot_size;
 	int	py;
 	int	px;
-} t_minimap;
+}	t_minimap;
 
-void mini_map(t_game *game);
-void draw_player_dot(t_game *game, t_minimap *v);
-void draw_minimap_grid(t_game *game, t_minimap *v);
-void init_minimap(t_minimap *v, t_game *game);
-
-
-void free_all_doors(t_door **head);
-void	open_the_door(t_game *game);
-void	add_door_to_end(t_door **head, int x, int y);
-
-
+void			mini_map(t_game *game);
+void			draw_player_dot(t_game *game, t_minimap *v);
+void			draw_minimap_grid(t_game *game, t_minimap *v);
+void			init_minimap(t_minimap *v, t_game *game);
+void			free_all_doors(t_door **head);
+void			open_the_door(t_game *game);
+void			add_door_to_end(t_door **head, int x, int y);
 size_t			ft_strlen(const char *s);
 void			ft_putstr_fd(char *s, int fd);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -237,17 +234,15 @@ size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char			*ft_strjoin(char const *s1, char const *s2, t_game *game);
 char			**ft_split(char const *s, char c, t_game *game);
 char			*ft_substr(char const *s, unsigned int start, \
-	size_t len, t_game *game);
+				size_t len, t_game *game);
 int				surrended_walls_check(char **arr);
 char			*ft_strchr(const char *s, int c);
 char			*get_next_line(int fd, t_game *game);
-// void			find_angle(t_game *game);
 unsigned int	parse_color_rgb(char *line);
 char			**parse_map(char *str, t_game *game);
 void			ft_replace_chr(char *s, char **as, char target, char new_chr);
 void			clean_parsing_stuff(t_game *game);
 void			init_my_struct(t_game *game);
-void			clean_parsing_stuff(t_game *game);
 void			clean_data(t_game *game);
 int				store_map_infos(t_game *game, char *line);
 int				is_valid_extension(char *filename, char *identifier);
@@ -255,54 +250,37 @@ void			*extract_and_store_data(int fd, t_game *game);
 void			ft_fix_sizes(char **arr, char c, t_game *game);
 int				validate_doors(char **arr);
 int				mouse_move(int x, int y, void *param);
-void			show_data_strored(t_game *game); /////////////////////
-void			init_logfile(void); ///////////////////////////
+void			show_data_strored(t_game *game);
+void			init_logfile(void);
 void			render_horse_sprite(t_game *game);
 void			animated_sprite(t_game *game);
-int				mouse_move(int x, int y, void *param);
 char			**padding_char_2d(char **map, t_game *game);
-int				validate_doors(char **arr);
 int				iter_flood_check(char **arr, int x, int y, t_game *game);
 int				is_wall(t_game *g, double x, double y);
-
-// double find_angle(char direction);
-void	free_2d_arr(char **str);
-
-void	init_game(t_game *g);
-/* Raycasting */
-void	raycast(t_game *g);
-void	init_ray(t_game *g, t_ray *r, int x);
-void	calc_ray_dir(t_game *g, t_ray *r);
-void	calc_step(t_game *g, t_ray *r);
-void	dda(t_game *g, t_ray *r);
-void	calc_wall_dist(t_game *g, t_ray *r);
-void	calc_wall_h(t_game *g, t_ray *r);
-void	draw_line(t_game *g, t_ray *r, int x);
-
-/* Rendering */
-void	load_texs(t_game *g);
-void	render(t_game *g);
-void	pixel_put(t_img *img, int x, int y, int color);
-
-/* Input */
-int		key_press(int key, t_game *g);
-int		key_release(int key, t_game *g);
-int		game_loop(t_game *g);
-int		close_win(t_game *g);
-
-/* Utils */
-
-void	error_exit(t_game *game, char *msg);
-void	*ft_memset(void *b, int c, size_t len);
-
-/* movment*/
-int		handle_movement(t_game *g, double move_speed);
-int		handle_rotation(t_game *g, double rot_speed);
-/*minimap*/
-
-void mini_map(t_game *game);
-
-void	handle_wasd(t_game *g, double move_speed, double *x, double *y);
-int	map_g(t_ray *r, t_game *g);
+void			free_2d_arr(char **str);
+void			init_game(t_game *g);
+void			raycast(t_game *g);
+void			init_ray(t_game *g, t_ray *r, int x);
+void			calc_ray_dir(t_game *g, t_ray *r);
+void			calc_step(t_game *g, t_ray *r);
+void			dda(t_game *g, t_ray *r);
+void			calc_wall_dist(t_game *g, t_ray *r);
+void			calc_wall_h(t_game *g, t_ray *r);
+void			draw_line(t_game *g, t_ray *r, int x);
+void			load_texs(t_game *g);
+void			render(t_game *g);
+void			pixel_put(t_img *img, int x, int y, int color);
+int				key_press(int key, t_game *g);
+int				key_release(int key, t_game *g);
+int				game_loop(t_game *g);
+int				close_win(t_game *g);
+void			error_exit(t_game *game, char *msg);
+void			*ft_memset(void *b, int c, size_t len);
+int				handle_movement(t_game *g, double move_speed);
+int				handle_rotation(t_game *g, double rot_speed);
+void			handle_wasd(t_game *g, double move_speed, double *x, double *y);
+int				map_g(t_ray *r, t_game *g);
+int				map_h(t_map map);
+int				map_w(t_map map);
 
 #endif
